72a328451976e55d9ff45a2357e19cf9557ee662
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-03-09 15:20:25 +0000

    Create welcome_user function

--- run.py
+++ run.py
@@ -4,6 +4,14 @@
 import requests
 from tkinter import *
 import math
+import random
 
 api_key = "e33690fb74cac3d02cc1384e38f73c7d"
 
+def welcome_user():
+    """Greets the user welcome and asks for their name."""
+    name = input("Hey there, windsurfer! What's your name? ")
+    print(f"Welcome to WaveRider, {name}!\n")
+
+
+welcome_user()
