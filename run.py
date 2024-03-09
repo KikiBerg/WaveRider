9dd633eefce35cec779a260e391af1c1a0aa0dfc
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-03-09 14:50:51 +0000

    Add modules, dependencies and api key from openweathermap

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+from tkinter import *
+import math
+
+api_key = "e33690fb74cac3d02cc1384e38f73c7d"
+
