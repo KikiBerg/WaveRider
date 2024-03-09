942d1042ee3be0e4202e9f635666395c8ec47451
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-03-09 15:44:32 +0000

    welcome_user function: add if, elif statemnets to check only letter input

--- run.py
+++ run.py
@@ -10,8 +10,21 @@ api_key = "e33690fb74cac3d02cc1384e38f73c7d"
 
 def welcome_user():
     """Greets the user welcome and asks for their name."""
-    name = input("Hey there, windsurfer! What's your name? ")
-    print(f"Welcome to WaveRider, {name}!\n")
+    print("Welcome to WaveRider! Your ultimate companion for conquering the windswept waves with finesse and confidence.\n")
+    while True:
+        name = input("Hey there, windsurfer! What's your name? ")
+
+        try:
+            if name.isalpha():                
+                print(f"Welcome to WaveRider, {name}!\n")
+                break
+            elif len(name) == 0:
+                print("Why not provide a name?")
+            else:
+                print(f"{name} is invalid. You can only enter letters a-z\n")
+        except ValueError as err:
+            print(f'{err}. Please try again.')           
+
 
 
 welcome_user()
