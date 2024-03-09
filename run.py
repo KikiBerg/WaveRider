38ee41b447b0e907f2f04dd5743460cad81df249
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-03-09 20:45:44 +0000

    Welcome user function: refactor and add input validation for the location prompt

--- run.py
+++ run.py
@@ -10,24 +10,35 @@ api_key = "e33690fb74cac3d02cc1384e38f73c7d"
 #location = "Stockholm, SE"
 
 def welcome_user():
-    """Greets the user welcome and asks for their name."""
-    print("Welcome to WaveRider! Your ultimate companion for conquering the windswept waves with finesse and confidence.\n")
-    while True:
-        name = input("Hey there, windsurfer! What's your name? ")
-
+    """Greets the user welcome, asks for their name and location and validates input."""
+    print("Welcome to WaveRider, your ultimate windsurfing companion!\n")
+    print("Hey there, windsurfer! Are you ready to conquer the waves today?\n")
+    
+    while True:  
         try:
-            if name.isalpha():                
-                print(f"Welcome to WaveRider, {name}!\n")
-                location = input(f"{name}, where are you located? (e.g., City, Country)\n")
-                print(f"Wow, {location} is such a nice place!\n")                
-                get_weather_data(api_key, location)  # Call get_weather_data function with user-provided location
-                break                                   
-            elif len(name) == 0:
-                print("Why not provide a name?")
-            else:
-                print(f"{name} is invalid. You can only enter letters a-z\n")
+            name = input("What's your name? ").upper()
+            if not name.isalpha():
+                raise ValueError("Name can only contain letters a-z.")
+            
+            while True:
+                location = input(f"{name}, where are you located? (e.g., City, Country)\n").upper()
+                try:
+                    city, country = location.strip().split(",")
+                    city = city.strip()
+                    country = country.strip()
+                    if not city or not country:
+                        raise ValueError("Please enter both city and country separated by a comma.\n")
+                    break
+                except ValueError:
+                    print("Invalid location format. Please enter: 'City, Country'.\n")
+
+            city_name = city # Extract city name into a variable           
+            print(f"Splendid! {city_name} is such a beautiful place!\n") # Modify the code to only show the city in the welcome message
+            get_weather_data(api_key, location)
+            break            
+
         except ValueError as err:
-            print(f'{err}. Please try again.')                 
+            print(f'{err}. Please try again.') 
 
 
 def get_weather_data(api_key, location):
@@ -37,15 +48,23 @@ def get_weather_data(api_key, location):
     response = requests.get(url).json() # convert it to a json in order to be able to access individual attributes
     
     if response['cod'] == 200: # Check if the request was successful (response code 200)
-        # Extract and display relevant weather information here, e.g:
+        # Extract and display relevant weather information here:
 
         weather = response['weather'][0]['main']
-        temperature = response['main']['temp']
-        print(f"The weather in {location} is currently {weather}. The temperature is {temperature}°C.")
+        temperature = response['main']['temp']        
+        feels_like = math.floor(response['main']['temp'])
+        print(f"The weather in {location.split(',')[0].strip()} is currently: {weather}. The temperature is {temperature}°C and it feels like {feels_like}°C")
     else:
         # Handle error: location not found, etc.
         print(f"An error occurred while fetching weather data for {location}.")
     
 
 
-welcome_user()
+
+
+def main():
+    """ Run all program functions """
+    welcome_user()
+    
+print("")
+main()
